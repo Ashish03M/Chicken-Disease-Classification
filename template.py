1c84e0f972197cd40fcdde8657d22ed0f0fb07a5
Ashish Mishragit config --global user.email ds.ashishmishra@gmail.com <ds.ashishmishra@gmail.com>
2024-03-11 17:10:32 +0530

    Folder structure added

--- template.py
+++ template.py
@@ -4,24 +4,25 @@ import logging
 
 logging.basicConfig(level=logging.INFO,format='[%(asctime)s]: %(message)s:')
 
-project_name='Chicken Disease Classifiction'
+project_name="Chicken Disease Classifiction"
 
 list_of_files=[
     ".github/workflows/.gitkeep",
-    f"src{project_name}/__init__.py",
-    f"src{project_name}/components/__init__.py",
-    f"src{project_name}/utils/__init__.py",
-    f"src{project_name}/config/__init__.py",
-    f"src{project_name}/config/configuration/__init__.py",
-    f"src{project_name}/pipeline/__init__.py",
-    f"src{project_name}/entity/__init__.py",
-    f"src{project_name}/constants/__init__.py",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration/__init__.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "dvc.yaml",
     "params.yaml",
     "requirements.txt",
     "setup.py",
-    "research/trial.ipynb"
+    "research/trial.ipynb",
+    "templates/index.html"
 ]
 
 for filepath in list_of_files:
