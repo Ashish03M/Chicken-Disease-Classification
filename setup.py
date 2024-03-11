58712c66ea32c2ef3722beac1acad4314be97cae
Ashish Mishragit config --global user.email ds.ashishmishra@gmail.com <ds.ashishmishra@gmail.com>
2024-03-11 17:45:50 +0530

    Requirements added

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ __version__ = "0.0.0"
 
 REPO_NAME = "Chicken-Disease-Classification"
 AUTHOR_USER_NAME = "Ashish03M"
-SRC_REPO = "Chicken Disease Classifiction"
+SRC_REPO = "Chciken_Disease_Classifier"
 AUTHOR_EMAIL = "ds.ashishmishra@gmail.com"
 
 
